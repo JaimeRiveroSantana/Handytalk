92aeeac909178e8e581e22d1d3b68ad70d753118
Olga Kalisiak <01178853@pw.edu.pl>
2024-12-15 14:51:28 +0000

    add lesson

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,2 +1,26 @@
 public class Lesson {
+    private String title;
+    private String content;
+
+    public Lesson(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void addContent(String additionalContent) {
+        this.content += "\n" + additionalContent;
+    }
+
+    public void displayLesson() {
+        System.out.println("Lekcja: " + title);
+        System.out.println("Treść: " + content);
+    }
 }
