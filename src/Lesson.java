e3fe567f7ea3c3d8a109de747ba0bc40644a47e3
fenfiera <nerea.vc1643@gmail.com>
2024-12-16 16:34:09 +0000

    added Olgas's code to my version

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,32 +1,60 @@
-class Lesson {
-    private String title;
-    private String description;
-    private boolean completed;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Lesson(String title, String description) {
+public class Lesson {
+    private String title;
+    private String content;
+    private List<String> quizQuestions;
+    private List<String> correctAnswers;
+    private int score;
+
+    public Lesson(String title, String content) {
         this.title = title;
-        this.description = description;
-        this.completed = false;
+        this.content = content;
+        this.quizQuestions = new ArrayList<>();
+        this.correctAnswers = new ArrayList<>();
+        this.score = 0;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getDescription() {
-        return description;
+    public String getContent() {
+        return content;
     }
 
-    public boolean isCompleted() {
-        return completed;
+    public void addContent(String additionalContent) {
+        this.content += "\n" + additionalContent;
     }
 
-    public void complete() {
-        completed = true;
+    public void addQuizQuestion(String question, String correctAnswer) {
+        quizQuestions.add(question);
+        correctAnswers.add(correctAnswer);
     }
 
-    @Override
-    public String toString() {
-        return "Lesson: " + title + ", Description: " + description + ", Completed: " + (completed ? "Yes" : "No");
+    public void answerQuiz(int questionIndex, String answer) {
+        if (questionIndex < quizQuestions.size()) {
+            if (correctAnswers.get(questionIndex).equalsIgnoreCase(answer)) {
+                System.out.println("Correct answer!");
+                score++;
+            } else {
+                System.out.println("Incorrect answer. Try again.");
+            }
+        } else {
+            System.out.println("There is no such question.");
+        }
+    }
+
+    public void displayLesson() {
+        System.out.println("Lesson: " + title);
+        System.out.println("Content: " + content);
+        System.out.println("Quiz results: " + score + "/" + quizQuestions.size());
+    }
+
+    public void displayQuiz() {
+        for (int i = 0; i < quizQuestions.size(); i++) {
+            System.out.println("Question " + (i + 1) + ": " + quizQuestions.get(i));
+        }
     }
 }
